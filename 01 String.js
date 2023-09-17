e8c2edc7c0a8f3b75863adf35e67365bd71d8678
pkumars397 <pkumars397@gmail.com>
2023-09-17 17:34:51 +0530

    Strings

---
+++ 01 String.js
@@ -0,0 +1,14 @@
+// ! DataTypes..Primitive >> Number,String,BigInt,Boolean,Null,Undefined,Symbol(Uniqueness).
+// ? Reference Datatypes >> Object,Array,Function.
+
+// * String declaration.
+
+// let str1="hello"
+// let str2 = new String("hello");
+// console.log(str1 === str2);//False ,because both are different datatypes.
+
+// let str1 = new String("hello");
+// let str2 = new String("hello");
+// console.log(str1==str2)//because object comparison is always wrong,reference datatypes(array,object,function)
+
+// todo ,console.log on console on browser to see all methods of string.
