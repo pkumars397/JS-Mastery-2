7dab999c67f82686469a6e48106ba8616e11ced7
pkumars397 <pkumars397@gmail.com>
2023-09-17 17:35:40 +0530

    Math Operation

---
+++ 02 Math.js
@@ -0,0 +1,7 @@
+let n = 2.56;
+// ! Math.round(),Math.ceil(),Math.floor(),Math.random(),Math.abs()
+console.log(n.toPrecision(2));
+console.log(n.toFixed(2));
+console.log(Math.round(n));
+console.log(Math.floor(Math.random() * 10 + 1)) //* 1 to 10;
+console.log(Math.floor(Math.random() * (6) + 5))//? min-max.. >> Math.random()*(max-min+1)+min
