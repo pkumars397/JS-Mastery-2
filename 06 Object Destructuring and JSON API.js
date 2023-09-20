2f9e3c7e6f288ca3232b634b0ef0b1404b0593d1
pkumars397 <pkumars397@gmail.com>
2023-09-20 18:16:25 +0530

    have a basic overview of Object Destructing and JSON

---
+++ 06 Object Destructuring and JSON API.js
@@ -0,0 +1,21 @@
+// const obj = { name: "prashant", age: 23, male: "male" };
+// ! Object Destructuring
+
+// const { name:meraNaam ,male,age} = obj;
+// console.log(meraNaam,male,age)
+
+// * What is API>>  Jab Apna Kaam Kisi aur ke sir pe Dalna ho.Haha.(Why JSON>>json object Notation ,API responces in terms of JSON)
+
+// !JSON Object(Two Formats)
+
+// {
+//     "firstName": "Prashant",
+//         "lastName": "Kumar",
+//         "age":24
+// }
+
+// [
+//     {},
+//     {},
+//     {}
+// ]
